0748cd15417217aa9764cfc2dcd32670bce6166d
jameskw <jameskw@compacsort.com>
2016-09-01 15:04:03 +0300

    efdsffdsf

---
+++ TeamCityCoverageTest/TeamCityCoverageTest/TeamCityCoverageTest.cpp
@@ -0,0 +1,23 @@
+// TeamCityCoverageTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
+int main()
+{
+	add(3, 5);
+	multiply(3, 5);
+	return 0;
+}
+
